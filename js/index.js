66609ff37f982f2bca98be58579ddbc663114f73
SlavikGemay <gemayit@gmail.com>
2024-12-15 23:30:18 +0200

    dz 5.1

---
+++ js/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+for (let num = 20; num >= 20 && num <= 30; num += 0.5) {
+    console.log(num);
+}
+
+
+// let num = 19.5;
+// do {
+//     num += 0.5;
+//     console.log(num);
+// }
+// while (num >= 19.5 && num < 30); // цикл з писляумовою, это я для себя
+
+
+
+// let num = 19.5;
+// while (num >= 19.5 && num <30) {   // цикл з передумовою
+//     num += 0.5;
+//     console.log(num);
+// }
+
